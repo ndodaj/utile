fa480c632bffabab2375561f2924006428a9eb44
nidodaj <nikoll.dodaj@eng.it>
2024-07-31 15:22:41 +0200

    delegate security to authentication server

--- src/main/java/al/utile/utile/config/SecurityConfig.java
+++ src/main/java/al/utile/utile/config/SecurityConfig.java
@@ -1,80 +1,17 @@
 package al.utile.utile.config;
 
-import al.utile.utile.security.JwtRequestFilter;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private JwtRequestFilter jwtRequestFilter;
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/authenticate", "/utile-test", "/helloEureka" ,  "users/register", "/swagger-ui/**", "/v3/api-docs/**", "/error").permitAll()
-                        .anyRequest().permitAll()
-                )
-                .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-//                .formLogin(Customizer.withDefaults())
-//                .oauth2Login(Customizer.withDefaults());
-
-//                .formLogin(formLogin -> formLogin
-//                        .loginPage("/login")
-//                        .permitAll()
-//                )
-//                .oauth2Login(oauth2Login -> oauth2Login
-//                        .loginPage("/oauth2/authorization/google")
-//                )
-        ;
-
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    ApplicationListener<AuthenticationSuccessEvent> authSuccess() {
-        return event -> {
-            Authentication authentication = event.getAuthentication();
-            LoggerFactory.getLogger(SecurityConfig.class).info("LOGIN SUCCESFUL [{}] - {}", authentication.getClass().getSimpleName(), authentication.getName());
-        };
-    }
-
 
 }
--- src/main/java/al/utile/utile/controller/AuthenticationController.java
+++ src/main/java/al/utile/utile/controller/AuthenticationController.java
@@ -2,12 +2,9 @@ package al.utile.utile.controller;
 
 
 import al.utile.utile.dto.AuthenticationRequest;
-import al.utile.utile.security.JwtUtil;
-import al.utile.utile.service.CustomUserDetailsService;
 import al.utile.utile.service.UserService;
 import al.utile.utile_common.utile.AuthenticationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,25 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
-
-    @Autowired
-    private JwtUtil jwtUtil;
-    @Autowired
     private UserService userService;
 
-
-
-//    @PostMapping("/authenticate")
-//    public ResponseEntity<UserDetails> loadUserByCredentials(@RequestBody AuthenticationRequest authenticationRequest) throws UsernameNotFoundException {
-//        return ResponseEntity.ok(userService.loadUserByCredentials(authenticationRequest));
-//    }
-
     @PostMapping("/authenticate")
-    public AuthenticationResponse loadUserByCredentials(@RequestBody AuthenticationRequest authenticationRequest) throws UsernameNotFoundException {
+    public AuthenticationResponse loadUserByCredentials(
+            @RequestBody AuthenticationRequest authenticationRequest) throws UsernameNotFoundException {
         return userService.loadUserByCredentials(authenticationRequest);
     }
 
