63fafd2e0f2780e8848d818ad292b4263e775356
nidodaj <nikoll.dodaj@eng.it>
2024-07-31 10:31:32 +0200

    working authentication api

--- src/main/java/al/utile/utile/service/UserService.java
+++ src/main/java/al/utile/utile/service/UserService.java
@@ -4,6 +4,7 @@ import al.utile.utile.converter.UserConverter;
 import al.utile.utile.dto.AuthenticationRequest;
 import al.utile.utile.entity.UserEntity;
 import al.utile.utile.repository.UserRepository;
+import al.utile.utile_common.utile.AuthenticationResponse;
 import al.utile.utile_common.utile.UserDto;
 import al.utile.utile_common.utile.UserRegistrationDto;
 import jakarta.validation.Valid;
@@ -16,6 +17,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -68,21 +71,15 @@ public class UserService {
         userRepository.save(userConverter.toEntity(userRegistrationDto));
     }
 
-    public UserDetails loadUserByCredentials(AuthenticationRequest authenticationRequest) {
+    public AuthenticationResponse loadUserByCredentials(AuthenticationRequest authenticationRequest) {
         UserEntity userEntity = userRepository.findByUsername(authenticationRequest.username())
                 .orElseThrow(() -> new BadCredentialsException("User ot found"));
-        String encode = bCryptPasswordEncoder.encode(authenticationRequest.password());
-        boolean matches = bCryptPasswordEncoder.matches(authenticationRequest.password(),
-                userEntity.getPassword()
-                );
-        if(!matches) {
+        if(!bCryptPasswordEncoder.matches(authenticationRequest.password(), userEntity.getPassword())) {
             throw new BadCredentialsException("Bad credentials");
         }
-        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
-        userEntity.getRoles().forEach(role -> {
-            authorities.add(new SimpleGrantedAuthority(role.getName().name()));
-        });
-        return new org.springframework.security.core.userdetails.User(userEntity.getUsername(), userEntity.getPassword(), authorities);
+        List<String> roles = new ArrayList<>();
+        userEntity.getRoles().forEach(role -> roles.add(role.getName().name()));
+        return new AuthenticationResponse(authenticationRequest.username(), roles);
 
 
 
